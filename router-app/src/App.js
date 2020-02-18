33004a810f428b488e6fcc16c1717d31c7b5e0be
Puneet Kumar <puneetkumar712@gmail.com>
2020-02-18 23:52:59 +0530

    routing done in router-app

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -7,7 +7,7 @@ import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -21,9 +21,12 @@ class App extends Component {
               path="/products"
               render={props => <Products sortBy="id" {...props} />}
             />
-            <Route path="/posts" component={Posts} />
+            <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
+            <Redirect from="/messages" to="posts"/>
+            <Route path="/not-found" component={NotFound} />
             <Route path="/" exact component={Home} />
+            <Redirect to="/not-found"/>
           </Switch>
         </div>
       </div>
