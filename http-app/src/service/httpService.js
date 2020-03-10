83bdb47043b6f8cb17f9762b8dd860e0bd2e3780
Puneet Kumar <puneetkumar712@gmail.com>
2020-03-11 00:03:46 +0530

    few more adding of http code

---
+++ http-app/src/service/httpService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import {toast} from "react-toastify"
+
+axios.interceptors.response.use(null, error => {
+    const expectedError =
+      error.response && error.response.status >= 400 && error.respnse.status < 500;
+  
+    if(!expectedError){
+      console.log("Logging error", error);
+      toast.error('Unexpected error occured')
+    }
+  
+    return Promise.reject(error);
+  });
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    patch: axios.patch,
+    delete: axios.delete
+}
